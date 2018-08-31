f29d299143999b8cc4bd7b6e0e580166df7427e9
Chris Larson <c.lars701@gmail.com>
2018-08-31 16:40:04 -0700

    add basic app functionality

--- database/index.js
+++ database/index.js
@@ -1,16 +1,60 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+// mongoose.connect('mongodb://localhost/fetcher');
+mongoose.connect('mongodb://localhost/test');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: {
+    type: Number,
+    unique: true
+  },
+  name: String,
+  fullname: String,
+  owner_id: Number,
+  owner_login: String,
+  htmlUrl: String,
+  description: String,
+  createdAt: Date,
+  updatedAt: Date,
+  watchers: Number,
+  issues: Number,
+  stars: Number,
+  forks: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (repos) => {
+  let reposSaved = 0;
+  let formattedRepos = [];
+  repos.forEach(repo => {
+    let formattedRepo = {
+      id: repo.id,
+      name: repo.name,
+      fullname: repo.fullname,
+      owner_id: repo.owner.id,
+      owner_login: repo.owner.login,
+      htmlUrl: repo.html_url,
+      description: repo.description,
+      createdAt: repo.created_at,
+      updatedAt: repo.updated_at,
+      watchers: repo.watchers_count,
+      stars: repo.stargazers_count,
+      issues: repo.open_issues_count,
+      forks: repo.forks
+    };
+    formattedRepos.push(formattedRepo);
+  });
+  Repo.insertMany(formattedRepos, { ordered: false }, (error, docs) => {
+    if (error) return console.log(error);
+    console.log(docs);
+    console.log('------------------');
+  });
+  // document.save((error, product) => {
+  //   if (error) return console.log(error);
+  //   reposSaved++;
+  //   console.log('product: ', product);
+  //   console.log('reposSaved: ', reposSaved);
+  // });
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
 const express = require('express');
+const db = require('../database/index');
+const github = require('../helpers/github');
+
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.json());
 
-app.post('/repos', function (req, res) {
+app.post('/repos/:username', function (req, res) {
+  let username = req.params.username;
+  github.getReposByUsername(username, (error, response) => {
+    if (error) return console.log(error);
+    let data = JSON.parse(response);
+    let repos = data.items;
+    db.save(repos, (error) => {
+      if (error) return console.log(err);
+      // console.log(reposSaved);
+    });
+    res.status(201).json({ ReposSubmitted: repos.length });
+  });
+
+  // writing some code here....
+  // doing some coding while screen capping.. testing out OBS!
+  // testing out a full screen recording now.... 
+
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
 });
+
+// https://api.github.com/search/repositories?q=user:ChrisALarson&sort=updated
+
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
